51ae361a0f85815e2e1d744d5057978d076b54c6
German C. Jimenez <German C. Jimenez@WIN-S9NDENHT7LQ>
2019-12-02 07:54:25 -0600

    Se crea la primera entidad para generaciòn de tablas en la base de datos.

---
+++ PROYECTO_DAL/CLS_DBC_CINE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_DAL
+{
+    public class CLS_DBC_CINE : DbContext
+    {
+        public DbSet<CLS_SALA> SALA { get; set; }
+
+    }
+}
